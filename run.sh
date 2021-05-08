a5a35d6c30b475f552ea756c39c70aa73d5c0a2c
Vitaly <vitalyb@gmail.com>
2021-05-09 00:33:38 +0300

    fix - broken by rename

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 REPLACE_PLACEHOLDER="_CONTENT_RENAMED"
 cp -R $1 $1$REPLACE_PLACEHOLDER
-node /Users/vitalyb/git/file-and-content-rename/index.js "$1$REPLACE_PLACEHOLDER" "$2"
+node $SCRIPT_DIR/index.js "$1$REPLACE_PLACEHOLDER" "$2"
 cd `dirName $1`
 NEW_FOLDER=`ls | grep $REPLACE_PLACEHOLDER`
 mv "${NEW_FOLDER}" "${NEW_FOLDER/$REPLACE_PLACEHOLDER/}"
