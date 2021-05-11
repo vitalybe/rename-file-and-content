bd8f7f434b8c6aacd009cb1681bf2044b2a6c704
Vitaly <vitalyb@gmail.com>
2021-05-12 01:17:07 +0300

    copy and rename options

--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # Usage
 
-`./run.sh "folder/with/files/tabDnsRouters" "dnsRouter -> healthCollector"`
+`./run.sh copy "folder/with/files/tabDnsRouters" "dnsRouter -> healthCollector"`
 
-This will change all the the files, folders and content with the word "dnsRouter" to "healthCollector".
+This will change all the files, folders and content with the word "dnsRouter" to "healthCollector".
+
+`./run.sh rename "folder/with/files/tabDnsRouters" "dnsRouter -> healthCollector"`
+
+This will do the same, but to a new folder that will be copied from the source.
 
--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-REPLACE_PLACEHOLDER="_CONTENT_RENAMED"
-cp -R $1 $1$REPLACE_PLACEHOLDER
-node $SCRIPT_DIR/index.js "$1$REPLACE_PLACEHOLDER" "$2"
-cd `dirName $1`
-NEW_FOLDER=`ls | grep $REPLACE_PLACEHOLDER`
-mv "${NEW_FOLDER}" "${NEW_FOLDER/$REPLACE_PLACEHOLDER/}"
+
+KIND=$1
+shift
+
+if [[ "$KIND" = "copy" ]]; then
+  REPLACE_PLACEHOLDER="_CONTENT_RENAMED"
+  cp -R $1 $1$REPLACE_PLACEHOLDER
+  node $SCRIPT_DIR/index.js "$1$REPLACE_PLACEHOLDER" "$2"
+  cd `dirName $1`
+  NEW_FOLDER=`ls | grep $REPLACE_PLACEHOLDER`
+  mv "${NEW_FOLDER}" "${NEW_FOLDER/$REPLACE_PLACEHOLDER/}"
+elif [[ "$KIND" = "rename" ]]; then
+  node $SCRIPT_DIR/index.js "$1" "$2"
+else
+  echo "Error: Invalid kind. Should be 'copy' or 'rename'"
+fi
